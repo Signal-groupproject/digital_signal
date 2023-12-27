05a0b4c1c9583abeea821636aa667d09a68c4388
Tang <100887811+wsyfine@users.noreply.github.com>
2023-12-27 20:13:41 +0800

    Update README.md

--- qt_demo/mainwindow.cpp
+++ qt_demo/mainwindow.cpp
@@ -16,18 +16,13 @@ mainwindow::~mainwindow() {
 
 void mainwindow::on_pushButton_clicked()
 {
-    // 读取图像
     Mat image = imread("D:\\lean.tif");
 
-    // 将OpenCV的Mat转换为Qt的QImage
     QImage qImage(image.data, image.cols, image.rows, image.step, QImage::Format_BGR888);
 
-    // 将QImage转换为QPixmap
     QPixmap pixmap = QPixmap::fromImage(qImage);
 
-    // 设置label的 pixmap
     imageLabel->setPixmap(pixmap);
 
-    // 调整label的大小以适应图像
     imageLabel->setFixedSize(pixmap.size());
 }
